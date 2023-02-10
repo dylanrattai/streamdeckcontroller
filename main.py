b85543727020a1424ea1dd6dd0d9a309a151c838
Dylan <73141996+dylanrattai@users.noreply.github.com>
2023-02-10 11:03:06 -0600

    some tgt stuff

--- main.py
+++ main.py
@@ -16,6 +16,33 @@ fellLow = false
 resetTgt = false
 
 #targeting ints (perspective in comments = looking at grid from field NOT DRIVER STATION)
-grid = None #0 = left outer grid, 1 = co-op grid, 2 = right outer grid
+grid = None #0 = left outer grid, 3 = co-op grid, 6 = right outer grid
 column = None #0 = leftmost column, 1 = middle column, 2 = rightmost column
 row = None #0 = lowest row, 1 = middle row, 2 = highest row
+tgt = [None, None] #Column, Row
+
+def setTgtInts():
+  if(grid0):
+    grid = 0
+  elif(grid1):
+    grid = 3
+  elif(grid2):
+    grid = 6
+  if(column0):
+    column = 0
+  elif(column1):
+    column = 1
+  elif(column2):
+    column = 2
+  if(row0):
+    row = 0
+  elif(row1):
+    row = 1
+  elif(row2):
+    row = 2
+
+def setTgt():
+  try:
+    tgt = [grid + column, row]
+  except:
+    return None
