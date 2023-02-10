f316b6902916a4f7c18a615531f98b3240501971
Dylan <73141996+dylanrattai@users.noreply.github.com>
2023-02-10 10:31:17 -0600

    setup some vars

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#button bools
+grid0 = false
+grid1 = false
+grid2 = false
+column0 = false
+column1 = false
+column2 = false
+row0 = false
+row1 = false
+row2 = false
+setTgt = false
+markGrid = false
+removeMark = false
+markPlaced = false
+fellLow = false
+resetTgt = false
 
+#targeting ints (perspective in comments = looking at grid from field NOT DRIVER STATION)
+grid = None #0 = left outer grid, 1 = co-op grid, 2 = right outer grid
+column = None #0 = leftmost column, 1 = middle column, 2 = rightmost column
+row = None #0 = lowest row, 1 = middle row, 2 = highest row
